3cdf10d246ec0d9e2f5a1f61a2be7e057cfe5f67
sai teja varma <saitejaandroid20@gmail.com>
2026-08-02 12:03:12 +0530

    commits for git q 1

---
+++ flask/backend/app.py
@@ -0,0 +1,36 @@
+import os
+
+from flask import Flask, jsonify, request
+from flask.cli import load_dotenv
+import pymongo
+
+
+load_dotenv()
+
+mongo_uri = os.getenv("mongo_uri")
+
+client = pymongo.MongoClient(mongo_uri)
+
+db = client["learning"]
+
+collection = db["sample_data"]
+
+app = Flask(__name__)
+
+@app.route('/register', methods=['POST'])
+def submit_data():
+    data = request.get_json()
+    print("received",data)
+    collection.insert_one(data)
+    return jsonify({"message": "Data submitted successfully"}), 201
+
+@app.route('/view', methods=['GET'])
+def view_data():
+    data = list(collection.find())
+    for item in data:
+        print(item)
+        del item["_id"]  # Remove the ObjectId field for JSON serialization
+    return jsonify(data), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=9000, debug=True)
